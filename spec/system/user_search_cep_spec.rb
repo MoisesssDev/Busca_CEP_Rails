651b0ab48732503be7545583ad60f5b5b72f86ad
Moises Almeida <moisesssprg@gmail.com>
2024-07-09 17:25:06 -0300

    test: adiciona teste para buscas invalidas

--- spec/system/user_search_cep_spec.rb
+++ spec/system/user_search_cep_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe "cep_search/index.html.tailwindcss", type: :feature do
     fill_in 'cep', with: '01311-000'
     click_button 'Buscar'
 
+    expect(page).to have_content('01311000')
     expect(page).to have_content('Avenida Paulista')
+    expect(page).to have_content('Bela Vista')
+    expect(page).to have_content('São Paulo')
+    expect(page).to have_content('SP')
+    expect(page).to have_content('11')   
+  end
+
+  it 'Busca por CEP inválido' do
+    visit root_path
+    fill_in 'cep', with: '00000-000'
+    click_button 'Buscar'
+
+    expect(page).to have_content('CEP inválido, tente digitar novamente.')
   end
 end
