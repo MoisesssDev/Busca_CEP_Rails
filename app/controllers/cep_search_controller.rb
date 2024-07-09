f823869ad1a219b4eaf7bd0833a9fb5d3a8cdae5
Moises Almeida <moisesssprg@gmail.com>
2024-07-09 15:28:41 -0300

    feat: adiciona a lógica de busca por CEP

--- app/controllers/cep_search_controller.rb
+++ app/controllers/cep_search_controller.rb
@@ -1,4 +1,14 @@
+require 'net/http'
+require 'json'
+
 class CepSearchController < ApplicationController
   def index
+    if params[:cep].present?
+      url = "https://cep.awesomeapi.com.br/json/#{params[:cep]}"
+      uri = URI(url)
+
+      response = Net::HTTP.get(uri)
+      @address = JSON.parse(response)
+    end
   end
 end
