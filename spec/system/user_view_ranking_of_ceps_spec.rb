ce0a74d7e4c2328ce99253cd5458eef86d2d5b8d
Moises Almeida <moisesssprg@gmail.com>
2024-07-10 15:26:37 -0300

    test: adiciona teste para a Quantidade de CEPs buscados por estado

--- spec/system/user_view_ranking_of_ceps_spec.rb
+++ spec/system/user_view_ranking_of_ceps_spec.rb
@@ -28,7 +28,7 @@ describe 'Usurario ver ranking de CEPs', type: :feature do
     expect(page).to have_content('Nenhum CEP foi buscado ainda.')
   end
 
-  it 'e ver os 3 mais buscados por estado' do
+  it 'e ver os 3 CEPs mais buscados de cada estado' do
     CepSearch.create(cep: '01311-000', count: 6, state: 'SP', city: 'São Paulo')
     CepSearch.create(cep: '01311-001', count: 6, state: 'RJ', city: 'Rio de Janeiro')
     CepSearch.create(cep: '01311-002', count: 6, state: 'SE', city: 'Aracaju')
@@ -45,5 +45,23 @@ describe 'Usurario ver ranking de CEPs', type: :feature do
     expect(page).not_to have_content('Fortaleza : 01311-003 - CE')
     expect(page).not_to have_content('Porto Alegre : 01311-004 - RS')
     expect(page).not_to have_content('Monteiro Lobato : 01311-005 - RJ')
+  end
+
+  it 'e ver a quantidade de CEPs buscados em cada estado' do
+    CepSearch.create(cep: '01311-000', count: 6, state: 'SP', city: 'São Paulo')
+    CepSearch.create(cep: '01311-001', count: 6, state: 'RJ', city: 'Rio de Janeiro')
+    CepSearch.create(cep: '01311-002', count: 6, state: 'SE', city: 'Aracaju')
+    CepSearch.create(cep: '01311-003', count: 5, state: 'CE', city: 'Fortaleza')
+    CepSearch.create(cep: '01311-004', count: 5, state: 'RS', city: 'Porto Alegre')
+    CepSearch.create(cep: '01311-005', count: 3, state: 'RJ', city: 'Monteiro Lobato')
+
+    visit root_path
+
+    expect(page).to have_content('Quantidade de CEPs buscados por estado')
+    expect(page).to have_content('SP : 1')
+    expect(page).to have_content('RJ : 2')
+    expect(page).to have_content('SE : 1')
+    expect(page).to have_content('CE : 1')
+    expect(page).to have_content('RS : 1')
   end
 end
