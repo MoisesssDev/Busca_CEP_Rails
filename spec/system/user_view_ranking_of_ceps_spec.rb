b1891b0a4966a3f6b1dd0c90926022233abd498e
Moises Almeida <moisesssprg@gmail.com>
2024-07-10 11:56:46 -0300

    test: refatora o teste para deixar mais legivel

--- spec/system/user_view_ranking_of_ceps_spec.rb
+++ spec/system/user_view_ranking_of_ceps_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'Usurario ver ranking de CEPs', type: :feature do
   it 'e ver os 5 mais buscados' do
-    CepSearch.create!(cep: '01311-000', city: 'São Paulo', state: 'SP', count: 5)
-    CepSearch.create!(cep: '01311-001', city: 'São Paulo', state: 'SP', count: 3)
-    CepSearch.create!(cep: '01311-002', city: 'São Paulo', state: 'SP', count: 2)
-    CepSearch.create!(cep: '01311-003', city: 'São Paulo', state: 'SP', count: 2)
-    CepSearch.create!(cep: '01311-004', city: 'São Paulo', state: 'SP', count: 2)
-    CepSearch.create!(cep: '01311-005', city: 'São Paulo', state: 'SP', count: 1)
+    CepSearch.create(cep: '01311-000', count: 5)
+    CepSearch.create(cep: '01311-001', count: 4)
+    CepSearch.create(cep: '01311-002', count: 3)
+    CepSearch.create(cep: '01311-003', count: 2)
+    CepSearch.create(cep: '01311-004', count: 1)
+    CepSearch.create(cep: '01311-005', count: 1)
 
     visit root_path
 
